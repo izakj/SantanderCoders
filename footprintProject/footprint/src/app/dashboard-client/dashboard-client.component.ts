e3a86c39e9c3947905c382f671da8293c466fcb8
izakj <izakjst@gmail.com>
2020-09-29 12:35:24 -0300

    Atualizacao grafico dashboard

--- footprintProject/footprint/src/app/dashboard-client/dashboard-client.component.ts
+++ footprintProject/footprint/src/app/dashboard-client/dashboard-client.component.ts
@@ -32,40 +32,55 @@ export class DashboardClientComponent implements OnInit {
   insertChart(){
     
     var myChart = new Chart('canvas', {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 000, 000, 000)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
+      type: 'bar',
+      data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março','Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [
+          {
+            label: 'Alojamento',
+            data: [5, 2, 5, 1, 3, 1, 0.25, 4, 9, 7, 3, 2],
+            backgroundColor: '#F94144',
+          },
+          {
+            label: 'Alimentação',
+            data: [7, 0.25, 3, 4, 0.25, 2, 0.25, 1, 1, 1, 2, 3],
+            backgroundColor: '#F3722C',
+          },
+          {
+            label: 'Transporte',
+            data: [1, 2, 8, 1, 0.1, 0.25, 0.25, 0.25, 0.5, 2, 3, 3],
+            backgroundColor: '#F9C74F',
+          },
+          {
+            label: 'Consumo',
+            data: [0.75, 2, 2, 3, 3, 0.75, 0.75, 4, 2, 2, 1, 4],
+            backgroundColor: '#90BE6D',
+          },
+          {
+            label: 'Resíduos',
+            data: [2, 0.5, 3.5, 4, 5, 0.1, 0.5, 4.5, 4, 2, 5, 1],
+            backgroundColor: '#577590',
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [{ 
+                    stacked: true,
+                    scaleLabel:{
+                      display: true,
+                      labelString: "Mês"
                     }
-                }]
-            }
+                  }],
+          yAxes: [{ 
+                    stacked: true,
+                    scaleLabel:{
+                      display: true,
+                      labelString: "Hectares Globais (gha)"
+                    }
+                  }]
         }
+      }
     });
   }
 
