069439d95c78761679615c616fcfa0831865f6ec
izakj <izakjst@gmail.com>
2020-10-05 10:15:30 -0300

    Pratica aula 02/10

---
+++ 02_Pratica_Aula/customer/Cliente/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {DropdownModule} from 'primeng/dropdown';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    DropdownModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
